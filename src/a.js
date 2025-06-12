dbfacdb9ed2fe24b4972838a9c491df7c22a2318
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-06-11 19:43:54 -0700

    get listing of vercel

--- src/a.js
+++ src/a.js
@@ -7,7 +7,7 @@ function getDirectories(source) {
     .map(dirent => dirent.name);
 }
 
-const directoryPath ='../../output'; // Replace with your directory path
+const directoryPath ='../..'; // Replace with your directory path
 const directories = getDirectories(directoryPath);
 console.log(directories);
 
