d72d6830c412d5590d35f95682dac4b3ee39906d
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-06-11 19:44:24 -0700

    i said vercel not /

--- src/a.js
+++ src/a.js
@@ -7,7 +7,7 @@ function getDirectories(source) {
     .map(dirent => dirent.name);
 }
 
-const directoryPath ='../..'; // Replace with your directory path
+const directoryPath ='../../vercel'; // Replace with your directory path
 const directories = getDirectories(directoryPath);
 console.log(directories);
 
