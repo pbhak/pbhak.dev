ceab19c035a125e66c0d5482e31b40b49c60dc26
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-06-30 00:12:29 -0700

    feat: markdown file scanning and generation

--- scripts/build.js
+++ scripts/build.js
@@ -2,45 +2,52 @@ const fs = require("fs");
 const path = require("path");
 const marked = require("marked");
 
-function getDirectories(source) {
-  return fs
-    .readdirSync(source, { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name);
-}
-
-// Define the file path (relative to the root of the project)
-const filePath = path.join(process.cwd() + "/public/writings", "hi.html");
-const inputPath = path.join(process.cwd() + '/writings', 'hi.md')
+// Template HTML file path for writings posts
 const templatePath = path.join(process.cwd() + "/scripts", "template.html");
 
-// Define the HTML content
-const content = marked.parse(fs.readFileSync(inputPath, 'utf8'));
+// Get thd base names of all Markdown files in /content/writings
+const writingsFiles = fs
+  .readdirSync(path.join(process.cwd(), "public"))
+  .filter((fileName) => fileName.endsWith(".md"))
+  .map((fileName) => path.basename(fileName, ".md"));
 
-// Ensure the "public" directory exists
-fs.mkdir(path.dirname(filePath), { recursive: true }, (err) => {
-  if (err) {
-    console.error("Error creating directory:", err);
-    return;
-  }
+console.info(
+  `Found ${writingsFiles.length} Markdown file(s) in /content/writings`
+);
 
-  fs.readFile(templatePath, (err, data) => {
+writingsFiles.forEach((fileName) => {
+  // Define the file path (relative to the root of the project)
+  const filePath = path.join(process.cwd() + "/public/writings", `${fileName}.html`);
+  const inputPath = path.join(process.cwd() + "/content/writings", `${fileName}.md`);
+
+  // Define the HTML content
+  const content = marked.parse(fs.readFileSync(inputPath, "utf8"));
+
+  // Ensure the "public" directory exists
+  fs.mkdir(path.dirname(filePath), { recursive: true }, (err) => {
     if (err) {
-      console.error("Error reading template:", err);
+      console.error("Error creating directory:", err);
       return;
     }
 
-    // Write the content to the file
-    fs.writeFile(
-      filePath,
-      data.toString().replace("<!-- CONTENT -->", content),
-      (writeErr) => {
-        if (writeErr) {
-          console.error("Error writing file:", writeErr);
-        } else {
-          console.log("File created successfully at", filePath);
-        }
+    fs.readFile(templatePath, (err, data) => {
+      if (err) {
+        console.error("Error reading template:", err);
+        return;
       }
-    );
+
+      // Write the content to the file
+      fs.writeFile(
+        filePath,
+        data.toString().replace("<!-- CONTENT -->", content),
+        (writeErr) => {
+          if (writeErr) {
+            console.error("Error writing file:", writeErr);
+          } else {
+            console.log("File created successfully at", filePath);
+          }
+        }
+      );
+    });
   });
 });
