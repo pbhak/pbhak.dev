460bfc4a8f0d90def16a136d89e6bd7cda41fd85
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-06-30 17:18:29 -0700

    add dynamic list population

--- content/writings/hi.md
+++ content/writings/hi.md
@@ -1,3 +1,8 @@
+---
+title: hi
+e: hi
+---
+
 # Hi!
 hey there! my name is parneel, but you probably know that already. this only exists to test my writings page functionality.
 if you see this, please message me asap to delete this.
--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,18 @@
 const fs = require("fs");
 const path = require("path");
 const marked = require("marked");
+const matter = require("gray-matter");
 
 // Template HTML file path for writings posts
-const templatePath = path.join(process.cwd() + "/scripts", "template.html");
+const templatePath = path.join(process.cwd(), "/scripts", "template.html");
+
+// Main /writings HTML file to be used for populating the main list
+const writingsIndexFilePath = path.join(
+  process.cwd(),
+  "public",
+  "writings",
+  "index.html"
+);
 
 // Get thd base names of all Markdown files in /content/writings
 const writingsFiles = fs
@@ -15,19 +24,26 @@ console.info(
   `Found ${writingsFiles.length} Markdown file(s) in /content/writings`
 );
 
+// <ul> element that will be dynamically populated, closed, then added to the main HTMLL file later
+const listElement = "<ul id='writings-list'>\n";
+
 writingsFiles.forEach((fileName) => {
   // Define the file path (relative to the root of the project)
-  const filePath = path.join(
-    process.cwd() + "/public/writings",
-    `${fileName}.html`
-  );
   const inputPath = path.join(
     process.cwd() + "/content/writings",
     `${fileName}.md`
   );
 
-  // Define the HTML content
-  const content = marked.parse(fs.readFileSync(inputPath, "utf8"));
+  // The matter object contains frontmatter as well as the Markdown data itself
+  const matterObject = matter(fs.readFileSync(inputPath, "utf8"));
+
+  const content = matterObject.content.trim(); // remove trailing newline at start of file
+  const data = matterObject.data; // Frontmatter
+
+  const filePath = path.join(
+    process.cwd() + "/public/writings",
+    `${data.slug ?? fileName}.html`
+  );
 
   // Ensure the "public" directory exists
   fs.mkdir(path.dirname(filePath), { recursive: true }, (err) => {
@@ -55,5 +71,32 @@ writingsFiles.forEach((fileName) => {
         }
       );
     });
+  });
+
+  // Create the HTML element and insert it in the list in /writings
+  listElement += `<li><a href="/writings/${data.slug ?? fileName}.html">${
+    data.title
+  }</a></li>\n`;
+});
+
+// Now that the list element is populated, close it
+listElement += "</ul>";
+
+// Add the list element to the file
+fs.readFile(writingsIndexFilePath, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading /writings/index.html: ", err);
+    return;
+  }
+
+  const replacedData = data.replace("<!-- LIST HERE -->", listElement);
+
+  fs.writeFile(writingsIndexFilePath, replacedData, (err) => {
+    if (err) {
+      console.error("Error writing to /writings/index.html: ", err);
+      return;
+    }
+
+    console.info("List data written to /writings/index.html!");
   });
 });
