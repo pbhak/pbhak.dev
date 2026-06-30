3a99ef798980fb750b0d025e15390df493576c9a
Parneel Bhakhri <hi@pbhak.dev>
2026-06-29 18:47:01 -0700

    add index at recipes and improve recipe schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,10 +2,21 @@ import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 import { defineCollection } from "astro:content";
 
+// TODO make quantity units an enum?
+const quantitySchema = z.object({ amount: z.number(), unit: z.string() })
+const ingredientSchema = z.object({
+  name: z.string(),
+  imperial: quantitySchema,
+  metric: quantitySchema.optional(),
+  note: z.string().optional(),
+  optional: z.boolean().default(false)
+})
+
 const recipes = defineCollection({
   loader: glob({ base: "./src/content/recipes", pattern: "**/*.{md, mdx}" }),
   schema: z.object({
-    name: z.string()
+    name: z.string(),
+    ingredients: z.array(ingredientSchema)
   }),
 });
 
